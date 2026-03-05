28ed208395a1d6c03d3eb5a93ff665a7a22a9241
indahnirmalazahranabil <83548180+indahnirmalazahranabil@users.noreply.github.com>
2026-03-05 11:45:56 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.praktam_2057051004
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktam_2057051004.ui.theme.PrakTAM_2057051004Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrakTAM_2057051004Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Indah Nirmala Zahra Nabil",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Halo saya Indah Nirmala Zahra Nabil dengan NPM 2057051004 siap belajar compose!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrakTAM_2057051004Theme {
+        Greeting("Android")
+    }
+}
